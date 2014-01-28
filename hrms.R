057aa1c78062baa943cf65070ffd9895c94460c2
Luke Marney <marneylc@gmail.com>
2014-01-28 11:31:15 +0000

    A new peak finder that calculates the m/z center of a peak by midpoint calculation from a horizontal line at half height. Also improvements to documentation.

--- hrms.R
+++ hrms.R
@@ -1,18 +1,17 @@
-# the target list must be in active directory and called LipidList.csv
-# to call from within R, execute the following 3 commands
-# source("./hrms")
-# main(filename,rt=c(0,60),mz=c(200,1800))
-# results <- snd()
-# 
-# if you want to run this for all data files in a directory run the following command after getting
-# the name of all files into the list variable "files"
-# system.time( 
-# for (i in 1:length(files)) { 
-#  main(files[i],rtwin=c(0,60),mzwin=c(200,1800))
-#}
-#)
-
-
+#############################################################################
+### The target list must be in active directory and called LipidList.csv. ###
+### To call from within R, execute the following two commands ###############
+# > source("hrms.R")
+# > main(filename,rtwin=c(0,60),mzwin=c(200,1800)) 
+# where rtwin is the retention time window in sec and mzwin is the mzwindow in m/z units
+#
+### If you want to run this for all data files in a directory run the following command after getting
+### the name of all files into the list variable "files"
+# > system.time( 
+# > for (i in 1:length(files)) { 
+# >   main(files[i],rtwin=c(0,60),mzwin=c(200,1800))
+# > }
+# >)
 main <- function(filename,rtwin,mzwin) {
   require(xcms)
   require(data.table)
@@ -25,7 +24,15 @@ main <- function(filename,rtwin,mzwin) {
   # but csv mode works
 }
 
-snd <- function() { # the csv files must be in the active directory
+#############################################################################
+### This function is used after main() has been run in a loop of multiple ###
+### filenames/spectra. It reads the multiple .csv's in a directory and ######
+### creates the .csv files signals.csv and deviations.csv which have ########
+### rownames of target metabolite and column names as the filename ##########
+### It's output is .csv files, so there are no arguments needed. The ########
+### active directory must be set to the directory of the csv files created ##
+### by main() ###############################################################
+signals_deviations <- function() { # the csv files must be in the active directory
   x <- read.csv(gsub(pattern=".mzXML", x=files[1], replacement=".csv"),header=T)
   targets <- read.table("./LipidList.csv" , header=T, sep=',')
   targets <- data.table(targets)
@@ -47,9 +54,14 @@ snd <- function() { # the csv files must be in the active directory
   return(results)
 }
 
+###################################################################################
+### this function uses xcms to get the average spectra for a sample ###############
+### and cleans up the spectra by rounding the ms data to 4 decimal places #########
+### as well as removing NA values. The function has three arguments, a file name ##
+### the retention time window that data will be averaged from #####################
+### the direct infusion of sample and the mass spectral range that is desired. ####
+### common values are: rt <- c(0,60) and mz <- c(200,1800) ########################
 getspectra <- function(filename,rt,mz) {
-  # rt <- c(0,60)
-  # mz <- c(200,1800)
   spectra <- list()
   spectrum <- getSpec(xcmsRaw(filename), rtrange=rt, mzrange=mz)
   spectrum[,"mz"] <- round(spectrum[,"mz"], digits=4)
@@ -60,12 +72,18 @@ getspectra <- function(filename,rt,mz) {
   return(spectrum)
 }
 
+################################################################
+### This function calls the peakfinding algorithm in a loop ####
+### finding the closest peak maximum m/z and intensity values ##
+### appending results to variables signals and nearest_mz. #####
+### It returns the data.frame peak_id which includes all #######
+### results from the targeted analysis of the current spectra ##
 peaktable <- function(targets,spectra) {
   nearest_mz <- vector(length=length(targets$mz)) #predefine length later
   signal <- vector(length=length(targets$mz)) #predefine length later
   for (i in 1:length(targets$mz)) {
     target <- targets[i,mz]
-    peak <- peakfind(target,spectra,0.01)
+    peak <- peakfind_midpoint(target,spectra,0.01)
     nearest_mz[i]<-peak[1,mz]
     signal[i]<-peak[1,V1]
   }
@@ -75,8 +93,37 @@ peaktable <- function(targets,spectra) {
   return(peak_id)
 }
 
+################################################################################################################
+### This is a refined peak finder that calculates the midpoint between a line drawn at the half height. This ###
+### will give closer m/z values, but does not correct any small errors in intensity calculations from the ######
+### peak max finder. Used in main() as of 28/01/2014. -Luke Marney #############################################
+peakfind_midpoint <- function(target,spectra,hwidth) {
+  window <- subset(spectra, spectra$mz > target-hwidth & spectra$mz < target+hwidth)
+  #plot(window, type='h', lwd=1)
+  setkey(window,V1) #this will sort table by intensity, thus finding peak maximum as last entry in table
+  peak <- window[length(window$mz)] #get last entry of table for the peak maximum
+  hh=peak[1,V1]/2
+  nearmz=peak[1,mz]
+  left_mzs <- c(max(subset(window, window$mz < nearmz & window$V1 < hh)$mz),min(subset(window, window$mz < nearmz & window$V1 > hh)$mz))
+  left_int <- c(max(subset(window, window$mz < nearmz & window$V1 < hh)$V1),min(subset(window, window$mz < nearmz & window$V1 > hh)$V1))
+  right_mzs <- c(max(subset(window, window$mz > nearmz & window$V1 < hh)$mz),min(subset(window, window$mz > nearmz & window$V1 > hh)$mz))
+  right_int <- c(max(subset(window, window$mz > nearmz & window$V1 < hh)$V1),min(subset(window, window$mz > nearmz & window$V1 > hh)$V1))
+  midpoints <- data.frame(left_mzs,left_int,right_mzs,right_mzs)
+  ### there has got to be a way to combine the last five rows into one row
+  coordinates <- list()
+  left <- coefficients(lm(left_int ~ left_mzs, data=midpoints))
+  right <- coefficients(lm(right_int ~ right_mzs, data=midpoints))
+  midpoint <- ((hh-left[1])/left[2]+(hh-right[1])/right[2])/2 # midpoint between the intersection points of both lines from a y=hh flat line
+  peak$mz <- round(midpoint[1], digits=4) # modify the peaks variable with the new more accurate m/z value
+  return(peak) 
+}
 
-peakfind <- function(target,spectra,hwidth) {
+###################################################
+### This function is the simplest peak finder. ####
+### It finds the maximum intensity in a defined  ##
+### window around the target m/z. Not used in #####
+### main() as of 28/01/2014. -Luke Marney #########
+peakfind_max <- function(target,spectra,hwidth) {
   window <- subset(spectra, spectra$mz > target-hwidth & spectra$mz < target+hwidth)
   setkey(window,V1) #this will sort table by intensity, thus finding peak maximum as last entry in table
   peak <- window[length(window$mz)] #get last entry of table for the peak maximum
